cb68949985f90c28060bab6f0522c144354592ad
Mario Figueiró Zemor <mario.figueiro@ufrgs.br>
2017-10-13 05:09:33 -0300

    Auto stash before merge of "master" and "origin/master"
    [change] Budgeted model

--- src/finantials/Budgeted.java
+++ src/finantials/Budgeted.java
@@ -1,21 +1,38 @@
 package finantials;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
 
 public class Budgeted extends AbstractBill {
 
-	private List<Rubric> rubrics;
+	private Map<Integer, Rubric> rubrics;
 
 	private Date month;
 	
-	
-
-	/**
-	 *  
-	 */
-	public void changeRubric(Rubric rubric) {
-
+	public Budgeted() {
+		this.rubrics = new HashMap<Integer, Rubric>();
 	}
-
+	
+	public Date getMonth() {
+		return this.month;
+	}
+	
+	public void setMonth(Date month) {
+		this.month = month;
+	}
+	
+	public void setRubric(Rubric rubric) {
+		this.rubrics.put(rubric.code, rubric);
+	}
+	
+	public Rubric getRubric(int code) {
+		return this.rubrics.get(code);
+	}
+	
+	public Collection<Rubric> getAllRubrics(){
+		return this.rubrics.values();
+	}
 }
