68b546a0985ded92e57c21fc52309e9bf5cb1d8f
Mario Figueiró Zemor <mario.figueiro@ufrgs.br>
2017-10-13 05:27:58 -0300

    [change] Realized model

--- src/finantials/Realized.java
+++ src/finantials/Realized.java
@@ -1,12 +1,44 @@
 package finantials;
 
-import java.util.List;
+import java.util.Map;
+import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
 
 public class Realized extends AbstractBill {
-
-	private List<Rubric> rubrics;
+	
+	private Map<Integer, Rubric> rubrics;
 
 	private Date month;
+	
+	public Realized() {
+		this.rubrics = new HashMap<Integer, Rubric>();
+	}
+	
+	public Date getMonth() {
+		return this.month;
+	}
+	
+	public void setMonth(Date month) {
+		this.month = month;
+	}
+	
+	public void setRubric(Rubric rubric) {
+		this.rubrics.put(rubric.code, rubric);
+	}
+	
+	public void setRubrics(Collection<Rubric> rubrics) {
+		rubrics.forEach((rubric) -> {
+			this.setRubric(rubric);
+		});
+	}
+	
+	public Rubric getRubric(int code) {
+		return this.rubrics.get(code);
+	}
+	
+	public Collection<Rubric> getAllRubrics(){
+		return this.rubrics.values();
+	}
 
 }
