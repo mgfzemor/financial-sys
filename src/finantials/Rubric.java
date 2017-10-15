5c191c1e550df27e6b09aa56e25ecc426f008a88
Mario Figueiró Zemor <mario.figueiro@ufrgs.br>
2017-10-15 02:32:07 -0200

    [change] RealizedYear
    compute result rubric's sum and enum rubricType definition.

--- src/finantials/Rubric.java
+++ src/finantials/Rubric.java
@@ -2,37 +2,65 @@ package finantials;
 
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Iterator;
 import java.util.Map;
 
 public class Rubric extends RubricBase {
 
-	private Map<Integer, Rubric> children;
+	private Map<Integer, Rubric> childrens;
 
 	private float value;
 
-	private Enum type;
+	private RubricType type;
 	
 	public Rubric() {
-		children = new HashMap<Integer, Rubric>();
+		childrens = new HashMap<Integer, Rubric>();
+	}
+	
+	public float getValue() {
+		return this.value;
+	}
+	
+	public void setValue(float value) {
+		this.value = value;
+	}
+	
+	public void setType(RubricType type) {
+		this.type = type;
 	}
 
-	/**
-	 *  
-	 */
-	public float CalcResult() {
-		return 0;
+	public RubricType getType() {
+		return this.type;
 	}
-
-	/**
-	 *  
-	 */
+	
 	public void setChildren(Rubric rubric) {
-		children.put(rubric.code , rubric);
+		childrens.put(rubric.code , rubric);
 	}
 	
 	public Collection<Rubric> getAllChildrens(){
-		return this.children.values();
+		return this.childrens.values();
 	}
 
+	private float sumRubrics(Collection<Rubric> rubrics) {
+		if (rubrics == null) {
+			return 0;
+		}
+		else {
+			float sum = 0;
+			for(Iterator<Rubric> iterator = rubrics.iterator(); iterator.hasNext();) {
+				Rubric rubric = iterator.next();
+				RubricType type= rubric.getType();
+				switch(type) {
+				case DEBIT: sum -= rubric.getValue();
+					break;
+				case CREDIT: sum += rubric.getValue();
+					break;
+				default:
+					break;
+				}
+				sum += this.sumRubrics(rubric.getAllChildrens());
+			}
+			return sum;
+		}
+	}
 }
